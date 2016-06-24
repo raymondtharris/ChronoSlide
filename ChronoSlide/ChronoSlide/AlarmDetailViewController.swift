e509d84feb1527604175a473dd144232fd6d3907
Tim Harris <raymondtimothyharris@gmail.com>
2016-06-23 22:33:39 -0700

    ViewController structure
    Restructuring all view controllers out of the central view controller
    file and making the view controllers not be repeated for new and edit
    workflows.

---
+++ ChronoSlide/ChronoSlide/AlarmDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AlarmDetailViewController.swift
+//  ChronoSlide
+//
+//  Created by Tim Harris on 6/23/16.
+//  Copyright © 2016 Tim Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class AlarmDetailViewController: UIViewController {
+    var isNewAlarm: Bool = true
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
