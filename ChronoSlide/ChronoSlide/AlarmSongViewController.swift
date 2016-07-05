547d5f0622daee1224a821573bb3c8879bd4ee60
Tim Harris <raymondtimothyharris@gmail.com>
2016-07-04 23:18:58 -0700

    Songsview restructure
    Working on adding in the framing for a better implementation of the
    songsviewcontroller.

--- ChronoSlide/ChronoSlide/AlarmSongViewController.swift
+++ ChronoSlide/ChronoSlide/AlarmSongViewController.swift
@@ -7,24 +7,46 @@
 //
 
 import Foundation
-import  UIKit
+import UIKit
+import MediaPlayer
 
 class AlarmSongViewController: UITableViewController {
     
+    @IBOutlet var alarmSelectedSongView: SelectedSongView!
+    
+    lazy var alarmSongLibrary:[MPMediaItem] = MPMediaQuery.songs().items!
     var isNewAlarm: Bool = true
     var alarmToConfig: Alarm = Alarm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if alarmToConfig.alarmSound == nil {
+            alarmSelectedSongView.isHidden = true
+        }
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+        return alarmSongLibrary.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AlarmSongTableCellView", for: indexPath)
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+class AlarmSongTableCellView: UITableViewCell {
+    @IBOutlet weak var alarmSongTextLabel: UILabel!
+    @IBOutlet weak var alarmSongImageView: UIImageView!
+    
+    var previewState: Bool = false
+    
+    
 }
