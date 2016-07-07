c36da1f96a138d431f83f60b20991ed7ee1cb04f
Tim Harris <raymondtimothyharris@gmail.com>
2016-07-06 22:40:51 -0700

    SongsViewController framing
    Setting up more code to work in the new restructure of the view
    controller classes.

--- ChronoSlide/ChronoSlide/AlarmSongViewController.swift
+++ ChronoSlide/ChronoSlide/AlarmSongViewController.swift
@@ -39,6 +39,38 @@ class AlarmSongViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         
+        if searchbarController.isActive && searchbarController.searchBar.text != "" {
+            cell.alarmSongTextLabel.text = filteredSongArray[(indexPath as NSIndexPath).row].title!
+            cell.alarmSongImageView.image = filteredSongArray[(indexPath as NSIndexPath).row].artwork?.image(at: cell.alarmSongImageView.frame.size)
+        } else {
+            cell.alarmSongTextLabel.text = songArray[(indexPath as NSIndexPath).row].title!
+            cell.alarmSongImageView.image = songArray[(indexPath as NSIndexPath).row].artwork?.image(at: cell.alarmSongImageView.frame.size)
+        }
+        cell.alarmSongImageView.isUserInteractionEnabled = true
+        
+        // Choose Song Gesture
+        let chooseGesture = UITapGestureRecognizer.init(target: self, action: #selector(AddSongsTableViewController.chooseSong(_:)))
+        self.view.addGestureRecognizer(chooseGesture)
+        
+        // Preview Song Gesutre
+        let previewGesture = UITapGestureRecognizer.init(target: self, action: #selector(AddSongsTableViewController.togglePreview(_:)))
+        cell.alarmSongImageView.addGestureRecognizer(previewGesture)
+    }
+    
+    
+}
+
+extension AlarmSongViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterSongs(searchController.searchBar.text!)
+    }
+    
+    func filterSongs(_ searchString: String){
+        //print(searchString)
+        filteredSongArray = songArray.filter { song in
+            return (song.title!.lowercased().contains(searchString.lowercased()))
+        }
+        tableView.reloadData()
     }
 }
 
